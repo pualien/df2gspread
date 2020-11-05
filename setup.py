436be23620b41012e1ffddaee565f3966942115e
msenardi <matteo.s@docsity.com>
2020-11-05 16:22:08 +0100

    upgraded requirements

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ __irequires__ = [
     # CORE DEPENDENCIES
     'argparse>=1.3.0',
     'gspread==3.6.0',
-    'google-api-python-client==1.12.5',
+    'google-api-python-client==1.10.0',
     'oauth2client>=1.5.0,<5.0.0dev',
     'pandas'
 ]
